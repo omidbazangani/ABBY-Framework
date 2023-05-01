f882c5c761dded8b1b9f7a923527d7cbd8aca3e3
Omid Bazangani <omid.bazangani@gmail.com>
2023-05-01 13:15:14 +0200

    remove radboud name from the files

--- abby/firmware/generate.py
+++ abby/firmware/generate.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-# Copyright (C) 2020-2021 Radboud University
+# Copyright (C) 2020-2021  
 # SPDX-License-Identifier: Apache-2.0
 
 # This script can be run independently from Abby.
--- tests/__init__.py
+++ tests/__init__.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2020-2021 Radboud University
+# Copyright (C) 2020-2021  
 # SPDX-License-Identifier: Apache-2.0
 
 """Test module for abby
